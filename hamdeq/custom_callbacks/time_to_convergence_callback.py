a72bc2de88b13b6a50d41f87455e3aaece2e5b4f
Cédric Goemaere <cedric.goemaere@ugent.be>
2023-11-27 06:10:19 -0700

    Fix memory leak at validation time

--- hamdeq/custom_callbacks/time_to_convergence_callback.py
+++ hamdeq/custom_callbacks/time_to_convergence_callback.py
@@ -11,26 +11,25 @@ class TimeToConvergenceCallback(Callback):
             )
 
         self.states = []
-        # Turn on state tracking
-        pl_module.deq.f_module.forward = track_states(pl_module.deq.f_module.forward, self.states)
-
-    def on_fit_end(self, trainer, pl_module):
-        # Turn off state tracking
-        pl_module.deq.f_module.forward = track_states(
-            pl_module.deq.f_module.forward, self.states, disable=True
-        )
 
     def on_train_batch_start(self, trainer, pl_module, batch, batch_idx):
         # Clear list before next batch starts
         self.states.clear()
 
+        # Turn on state tracking
+        pl_module.deq.f_module.forward = track_states(pl_module.deq.f_module.forward, self.states)
+
     @torch.no_grad()
     def on_train_batch_end(self, trainer, pl_module, outputs, batch, batch_idx):
+        # Calculate relative residual
         states = torch.stack(self.states, dim=0)
         rel_residual = (  # shape=(nr_of_time_steps-1, batch_size)
             torch.linalg.vector_norm(states.diff(dim=0), ord=2, dim=-1)
             / torch.linalg.vector_norm(states[1:], ord=2, dim=-1)
         ).float()  # no-op to get good lay-out
+
+        # Clear list after it's been processed
+        self.states.clear()
 
         # We define convergence as having a rel_residual below 1e-4
         # and track the first time this happens
@@ -38,7 +37,7 @@ class TimeToConvergenceCallback(Callback):
 
         # Careful: if a sample did not converge, converged_indx will now be 0
         did_not_converge = converged_indx == 0
-        self.log("Did not converge", did_not_converge.float().sum())
+        self.log("Did not converge", did_not_converge.int().sum())
         converged_indx[did_not_converge] = rel_residual.size(0)  # =max number of time steps
 
         # Calculate equivalent forward Euler ODE solver time
@@ -46,6 +45,11 @@ class TimeToConvergenceCallback(Callback):
         step_size = 1.0 - pl_module.deq.config["damping_factor"]
         time_to_convergence = converged_indx * step_size
         self.log("Time to convergence", time_to_convergence)
+
+        # Turn off state tracking
+        pl_module.deq.f_module.forward = track_states(
+            pl_module.deq.f_module.forward, self.states, disable=True
+        )
 
     ### Test time code ###
 
@@ -58,6 +62,3 @@ class TimeToConvergenceCallback(Callback):
     def on_test_batch_end(self, trainer, pl_module, batch, batch_idx, dataloader_idx=0):
         # Now, self.log does on_epoch=True accumulation automatically
         self.on_train_batch_end(trainer, pl_module, None, batch, batch_idx)
-
-    def on_test_end(self, trainer, pl_module):
-        self.on_fit_end(trainer, pl_module)
--- hamdeq/custom_callbacks/track_log_relative_residual_callback.py
+++ hamdeq/custom_callbacks/track_log_relative_residual_callback.py
@@ -11,31 +11,36 @@ class TrackLogRelativeResidualCallback(Callback):
             )
 
         self.states = []
-        # Turn on state tracking
-        pl_module.deq.f_module.forward = track_states(pl_module.deq.f_module.forward, self.states)
 
         # Make list of all relative residuals
         self.all_rel_res = []
-
-    def on_fit_end(self, trainer, pl_module):
-        # Turn off state tracking
-        pl_module.deq.f_module.forward = track_states(
-            pl_module.deq.f_module.forward, self.states, disable=True
-        )
 
     def on_train_batch_start(self, trainer, pl_module, batch, batch_idx):
         # Clear list before next batch starts
         self.states.clear()
 
+        # Turn on state tracking
+        pl_module.deq.f_module.forward = track_states(pl_module.deq.f_module.forward, self.states)
+
     @torch.no_grad()
     def on_train_batch_end(self, trainer, pl_module, outputs, batch, batch_idx):
+        # Calculate relative residual
         states = torch.stack(self.states, dim=0)
         rel_residual = (  # shape=(nr_of_time_steps-1, batch_size)
             torch.linalg.vector_norm(states.diff(dim=0), ord=2, dim=-1)
             / torch.linalg.vector_norm(states[1:], ord=2, dim=-1)
         ).log10()
 
+        # Clear list after it's been processed
+        self.states.clear()
+
+        # Add relative residual to list of all residuals
         self.all_rel_res.append(rel_residual.cpu())
+
+        # Turn off state tracking
+        pl_module.deq.f_module.forward = track_states(
+            pl_module.deq.f_module.forward, self.states, disable=True
+        )
 
     ### Test time code ###
 
@@ -47,6 +52,3 @@ class TrackLogRelativeResidualCallback(Callback):
 
     def on_test_batch_end(self, trainer, pl_module, batch, batch_idx, dataloader_idx=0):
         self.on_train_batch_end(trainer, pl_module, None, batch, batch_idx)
-
-    def on_test_end(self, trainer, pl_module):
-        self.on_fit_end(trainer, pl_module)
